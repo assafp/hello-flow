f4af305c96b2ae0c5d8d47da00ff5b465c399cd6
Assaf <assaf@Assafs-Laptop.local>
2026-01-27 18:46:25 +0200

    access results bucket

--- flow.py
+++ flow.py
@@ -1,5 +1,6 @@
 from prefect import flow
-
+from prefect_gcp.cloud_storage import GcsBucket
+gcp_cloud_storage_bucket_block = GcsBucket.load("gcs-results")
 
 @flow(log_prints=True, persist_result=True)
 def my_flow(name: str = "world"):
